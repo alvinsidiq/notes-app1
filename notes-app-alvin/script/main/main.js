09ca72153fb0daa482a8316147e9a21b64eaca1f
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2024-04-01 10:54:14 +0700

    Update main.js

--- notes-app-alvin/script/main/main.js
+++ notes-app-alvin/script/main/main.js
@@ -1,15 +1,20 @@
 import "../componenents/app-bar.js";
 import "../componenents/form-input.js";
-import"../componenents/note-list.js";
-import"../componenents/search-note.js";
-const noteList = document.querySelector("note-list");
-noteList.note = notesData;
+import "../componenents/note-list.js";
+import "../componenents/search-note.js";
+import notesData from "../data/notes-data.js";
 
-customElements.whenDefined("form-input").then(() =>{
-    document
-    .querySelector("form-input")
-    .addEventListener("submit-button",(event) =>{
+// Menunggu definisi elemen custom 'note-list'
+customElements.whenDefined("note-list").then(() => {
+    const noteList = document.querySelector("note-list");
+    noteList.note = notesData;
+});
+
+customElements.whenDefined("form-input").then(() => {
+    document.querySelector("form-input").addEventListener("submit-button", (event) => {
         notesData.push(event.detail);
+        // Setelah menambahkan data baru, perbarui properti 'note' di 'note-list'
+        const noteList = document.querySelector("note-list");
         noteList.note = notesData;
-    })
-})
+    });
+});
